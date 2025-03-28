051959ba71f6e88160ce328d8e712d0e2b10557f
mrrajabali <mrrajabali@gmail.com>
2025-03-29 00:22:40 +0330

    feat : implement main Form with input event handling

--- CSharp-basic-Project1-CleanArch_Calculator/Ui/Form1.cs
+++ CSharp-basic-Project1-CleanArch_Calculator/Ui/Form1.cs
@@ -1,15 +1,35 @@
+using CSharp_basic_Project1_CleanArch_Calculator.App.Services;
+
 namespace CSharp_basic_Project1_CleanArch_Calculator
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        private readonly InputEventHandler _inputEventHandler;
+
+        public Form1(InputEventHandler inputEventHandler)
         {
+            _inputEventHandler = inputEventHandler;
             InitializeComponent();
 
-            this.KeyDown +=  
+            this.KeyPreview = true;
+            this.txtInput.Enabled = false;
+            this.txtResult.Enabled = false;
+
+            this.KeyDown += _inputEventHandler.KeyDownEventHandler;
+
+            foreach (Control control in this.Controls)
+            {
+                if (control is Button)
+                {
+                    control.Click += _inputEventHandler.ButtonHandler;
+                }
+            }
         }
 
-
-
+        private void UpdateTextBoxes(string inputData, string resultData)
+        {
+            txtInput.Text = inputData;
+            txtResult.Text = resultData;
+        }
     }
 }
