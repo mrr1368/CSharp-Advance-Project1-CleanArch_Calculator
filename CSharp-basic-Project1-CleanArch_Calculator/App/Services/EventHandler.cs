d001beedbb791b0f4952e27327970b9b61c707b9
mrrajabali <mrrajabali@gmail.com>
2025-03-28 23:35:37 +0330

    feat : impliment event handler services in app layer

---
+++ CSharp-basic-Project1-CleanArch_Calculator/App/Services/EventHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_basic_Project1_CleanArch_Calculator.App.Services
+{
+    public class EventHandler
+    {
+        public void KeyDownEventHandler(object? sender, KeyEventArgs e)
+        {
+            //TODO :
+        }
+
+        public void ButtonHandler(object? sender, EventArgs e)
+        {
+            //TODO :
+        }
+    }
+}
