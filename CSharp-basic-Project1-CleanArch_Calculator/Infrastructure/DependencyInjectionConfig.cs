837b8adf69648b8d379ebb3a960846f310faebe1
mrrajabali <mrrajabali@gmail.com>
2025-03-28 21:07:30 +0330

    feat : impliment DIP extention  for centrilize DI management

---
+++ CSharp-basic-Project1-CleanArch_Calculator/Infrastructure/DependencyInjectionConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_basic_Project1_CleanArch_Calculator.Infrastructure
+{
+    class DependencyInjectionConfig
+    {
+    }
+}
