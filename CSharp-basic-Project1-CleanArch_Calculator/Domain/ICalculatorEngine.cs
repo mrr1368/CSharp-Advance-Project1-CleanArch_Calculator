119a528041cc9fb6f582a72f51458cfa3d725669
mrrajabali <mrrajabali@gmail.com>
2025-03-29 13:19:21 +0330

    feat : add ICalculationEngine Interface for evaluation

---
+++ CSharp-basic-Project1-CleanArch_Calculator/Domain/ICalculatorEngine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_basic_Project1_CleanArch_Calculator.Domain
+{
+    public interface ICalculatorEngine
+    {
+        string Evaluate(string expression);
+    }
+}
