28719b478127a0166468bb96f909060f97789d08
mrrajabali <mrrajabali@gmail.com>
2025-04-10 15:05:21 +0330

    feat: extend existing feature with additional services

--- CSharp-basic-Project1-CleanArch_Calculator/Infrastructure/DependencyInjectionConfig.cs
+++ CSharp-basic-Project1-CleanArch_Calculator/Infrastructure/DependencyInjectionConfig.cs
@@ -1,4 +1,5 @@
 ﻿using CSharp_basic_Project1_CleanArch_Calculator.App.Services;
+using CSharp_basic_Project1_CleanArch_Calculator.Domain;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,10 @@ namespace CSharp_basic_Project1_CleanArch_Calculator.Infrastructure
         public static void RegisterServices(this IServiceCollection services)
         {
             services.AddSingleton<InputEventHandler>();
+            services.AddSingleton<KeyboardInputHandler>();
+            services.AddSingleton<ButtonHandler>();
+            services.AddSingleton<InputProcessor>();
+            services.AddSingleton<ICalculateEngine, ComputeCalculateEngine>();
         }
     }
 }
