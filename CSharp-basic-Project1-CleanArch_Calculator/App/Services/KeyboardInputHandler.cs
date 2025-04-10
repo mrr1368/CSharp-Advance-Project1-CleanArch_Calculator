091740ca2a2b8c5f586eeda89063b8d42660c8bf
mrrajabali <mrrajabali@gmail.com>
2025-04-10 14:56:02 +0330

    refactor: restructure key input conversion with shift support preparation

--- CSharp-basic-Project1-CleanArch_Calculator/App/Services/KeyboardInputHandler.cs
+++ CSharp-basic-Project1-CleanArch_Calculator/App/Services/KeyboardInputHandler.cs
@@ -14,6 +14,30 @@ namespace CSharp_basic_Project1_CleanArch_Calculator.App.Services
             //TODO 
         }
 
+        private string ConvertKeyInpu(Keys keycode) => keycode switch
+        {
+            Keys.D0 or Keys.NumPad0 => "0",
+            Keys.D1 or Keys.NumPad1 => "1",
+            Keys.D2 or Keys.NumPad2 => "2",
+            Keys.D3 or Keys.NumPad3 => "3",
+            Keys.D4 or Keys.NumPad4 => "4",
+            Keys.D5 or Keys.NumPad5 => "5",
+            Keys.D6 or Keys.NumPad6 => "6",
+            Keys.D7 or Keys.NumPad7 => "7",
+            Keys.D8 or Keys.NumPad8 => "8",
+            Keys.D9 or Keys.NumPad9 => "9",
+            Keys.Decimal => ".",
+            Keys.Add => "+",
+            Keys.Subtract => "-",
+            Keys.Multiply => "*",
+            Keys.Divide => "/",
+            Keys.Enter => "ENTER",
+            Keys.Back => "BACKSPACE",
+            Keys.Delete => "DELETE",
+            Keys.Escape => "ESCAPE",
+             _ => string.Empty,
+        };
+
         private string ConvertKeyToInput(Keys keyCode, bool isShiftPressed)
         {
             switch (keyCode)
