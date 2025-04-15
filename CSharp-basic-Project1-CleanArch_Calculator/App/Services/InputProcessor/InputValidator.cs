d0324e1ba064c725cfa7619bae7b4c93cf1f8a6e
mrrajabali <mrrajabali@gmail.com>
2025-04-15 23:39:29 +0330

    Add InputValidator class for input validation logic
    This commit introduces the `InputValidator` class in the
    `CSharp_basic_Project1_CleanArch_Calculator.App.Services.InputProcessor`
    namespace. The class features the `CanAddInput` method, which
    validates new inputs based on rules for digits and operators,
    ensuring that mathematical expressions are correctly formatted.

---
+++ CSharp-basic-Project1-CleanArch_Calculator/App/Services/InputProcessor/InputValidator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_basic_Project1_CleanArch_Calculator.App.Services.InputProcessor
+{
+    public class InputValidator
+    {
+        private static readonly char[] Operator = { '+', '-', '*', '/' };
+
+        public bool CanAddInput(string currentInput, string newInput)
+        {
+            if (string.IsNullOrEmpty(currentInput))
+                return !string.IsNullOrEmpty(newInput) && char.IsDigit(newInput[0]);
+
+            char lastChar = currentInput[^1];
+
+            if (newInput == ".")
+            {
+                int lastOperatorIndex = currentInput.LastIndexOfAny(Operator);
+
+                string lastNumberPart = lastOperatorIndex == -1 ? currentInput : currentInput[(lastOperatorIndex + 1)..];
+
+                if (lastNumberPart.Contains('.')) return false;
+
+                if (lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '/')
+                    return false;
+
+                return true;
+            }
+
+            if (currentInput.Length > 0 && char.IsDigit(currentInput[0])) return true;
+
+            if (Operator.Contains(newInput[0]))
+            {
+                if (Operator.Contains(lastChar)) return false;
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
