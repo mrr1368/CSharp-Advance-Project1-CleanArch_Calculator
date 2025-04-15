1655c693ee943dcb631c8e98e64ce1cbc6cc5132
mrrajabali <mrrajabali@gmail.com>
2025-04-15 23:41:09 +0330

    Enhance service registrations in DependencyInjectionConfig
    Updated `RegisterServices` method to add new services:
    `Form1`, `InputEventHandler`, `KeyboardInputHandler`,
    `ButtonInputHandler`, `DisplayManager`, `InputProcessorTest`,
    `InputValidator`, and `ExitProcessor`. Replaced
    `ButtonHandler` with `ButtonInputHandler`. Added
    necessary using directives for the new services.

--- CSharp-basic-Project1-CleanArch_Calculator/Infrastructure/DependencyInjectionConfig.cs
+++ CSharp-basic-Project1-CleanArch_Calculator/Infrastructure/DependencyInjectionConfig.cs
@@ -1,4 +1,8 @@
-﻿using CSharp_basic_Project1_CleanArch_Calculator.App.Services;
+﻿using CSharp_basic_Project1_CleanArch_Calculator.App.Services.CalculateEngine;
+using CSharp_basic_Project1_CleanArch_Calculator.App.Services.DisplayManager;
+using CSharp_basic_Project1_CleanArch_Calculator.App.Services.InputEventHandler;
+using CSharp_basic_Project1_CleanArch_Calculator.App.Services.InputHandler;
+using CSharp_basic_Project1_CleanArch_Calculator.App.Services.InputProcessor;
 using CSharp_basic_Project1_CleanArch_Calculator.Domain;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -13,11 +17,17 @@ namespace CSharp_basic_Project1_CleanArch_Calculator.Infrastructure
     {
         public static void RegisterServices(this IServiceCollection services)
         {
+            services.AddSingleton<Form1>();
             services.AddSingleton<InputEventHandler>();
             services.AddSingleton<KeyboardInputHandler>();
-            services.AddSingleton<ButtonHandler>();
+            services.AddSingleton<ButtonInputHandler>();
             services.AddSingleton<InputProcessor>();
+            services.AddSingleton<DisplayManager>();
             services.AddSingleton<ICalculateEngine, ComputeCalculateEngine>();
+
+            services.AddSingleton<InputProcessorTest>();
+            services.AddSingleton<InputValidator>(); 
+            services.AddSingleton<ExitProcessor>(); 
         }
     }
 }
