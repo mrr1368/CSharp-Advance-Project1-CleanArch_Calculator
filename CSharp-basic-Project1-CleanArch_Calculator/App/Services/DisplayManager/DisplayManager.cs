1a63a96e50ec2976016c30827251ebe6cb6879fa
mrrajabali <mrrajabali@gmail.com>
2025-04-15 22:46:14 +0330

    Add DisplayManager class for input and result display
    Introduces the `DisplayManager` class in the
    `CSharp_basic_Project1_CleanArch_Calculator.App.Services.DisplayManager`
    namespace. This class manages the display of input and result data
    using an `InputProcessor` instance. It includes methods to
    retrieve and return input and result data.

---
+++ CSharp-basic-Project1-CleanArch_Calculator/App/Services/DisplayManager/DisplayManager.cs
@@ -0,0 +1,28 @@
+﻿using CSharp_basic_Project1_CleanArch_Calculator.App.Services.InputProcessor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_basic_Project1_CleanArch_Calculator.App.Services.DisplayManager
+{
+    public class DisplayManager(InputProcessorTest inputProcessor)
+    {
+        private readonly InputProcessorTest _inputProcessor = inputProcessor;
+
+
+        public string DisplayInputData()
+        {
+            string inputData = _inputProcessor.GetInput();
+            return inputData;
+        }
+
+        public string DisplayResultData()
+        {
+            string resultData = _inputProcessor.GetResult();
+            return resultData;
+        }
+
+    }
+}
