6c4ad0c6a3d2027c613c44578cc01d79f77be0d5
mrrajabali <mrrajabali@gmail.com>
2025-04-15 23:39:08 +0330

    Add ExitProcessor class for exit request handling
    Introduces the `ExitProcessor` class in the
    `CSharp_basic_Project1_CleanArch_Calculator.App.Services.InputProcessor` namespace.
    This class features an `ExitRequested` event and an `ExitApp()` method
    to facilitate exit requests in the application.

---
+++ CSharp-basic-Project1-CleanArch_Calculator/App/Services/InputProcessor/ExitProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharp_basic_Project1_CleanArch_Calculator.App.Services.InputProcessor
+{
+    public class ExitProcessor
+    {
+        public event Action? ExitRequested;
+
+        public void ExitApp()
+        {
+            ExitRequested?.Invoke();
+        }
+    }
+}
