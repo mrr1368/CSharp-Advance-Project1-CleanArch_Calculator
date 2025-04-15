d779fb85b1a8c22d8d4690132c8a7d527955bb30
mrrajabali <mrrajabali@gmail.com>
2025-04-15 23:37:26 +0330

    Add InputEventHandler class for input event management
    This commit introduces the `InputEventHandler` class in the
    `CSharp_basic_Project1_CleanArch_Calculator.App.Services.InputEventHandler`
    namespace. The class handles input events from keyboard and button sources,
    utilizing `KeyboardInputHandler`, `ButtonInputHandler`, and `DisplayManager`
    through dependency injection. It defines an `OnDataProcessed` event and
    includes methods for processing keyboard and button events, which invoke
    data retrieval and processing from the `DisplayManager`.

---
+++ CSharp-basic-Project1-CleanArch_Calculator/App/Services/InputEventHandler/InputEventHandler.cs
@@ -0,0 +1,48 @@
+﻿using CSharp_basic_Project1_CleanArch_Calculator.App.Services.InputHandler;
+using CSharp_basic_Project1_CleanArch_Calculator.App.Services.DisplayManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_basic_Project1_CleanArch_Calculator.App.Services.InputEventHandler
+{
+    public class InputEventHandler(KeyboardInputHandler keyboardInputHandler, ButtonInputHandler buttonInputHandler,
+                                       DisplayManager displayManager)
+    {
+        private readonly KeyboardInputHandler _keyboardInputHandler = keyboardInputHandler;
+        private readonly ButtonInputHandler _buttonInputHandler = buttonInputHandler;
+        private readonly DisplayManager _displayManager = displayManager;
+
+        public event Action<string, string>? OnDataProcessed;
+
+        public void KeyDownEventHandler(object? sender, KeyEventArgs e)
+        {
+            if (_keyboardInputHandler.KeyboardManager(e.KeyCode))
+            {
+                ProcessData();
+            }
+        }
+
+        public void ButtonHandler(object? sender, EventArgs e)
+        {
+            if (sender == null)
+            {
+                MessageBox.Show("Error: The sender cannot be null. Please try again.",
+                        "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; 
+            }
+
+            _buttonInputHandler.ButtonManager(sender);
+            ProcessData();
+        }
+
+        private void ProcessData()
+        {
+            string inputData = _displayManager.DisplayInputData();
+            string resultData = _displayManager.DisplayResultData();
+            OnDataProcessed?.Invoke(inputData, resultData);
+        }
+    }
+}
