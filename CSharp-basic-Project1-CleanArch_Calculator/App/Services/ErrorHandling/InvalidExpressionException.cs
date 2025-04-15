805364c626285b38d45e1f8f9e36cde0b8039a64
mrrajabali <mrrajabali@gmail.com>
2025-04-15 22:43:20 +0330

    feat: Add InvalidExpressionException class for error handling
    Introduces a new `InvalidExpressionException` class that extends the base `Exception` class, allowing for custom error messages. The class is located in the `CSharp_basic_Project1_CleanArch_Calculator.App.Services.ErrorHandling` namespace. Several `using` directives have also been added for necessary namespaces.

---
+++ CSharp-basic-Project1-CleanArch_Calculator/App/Services/ErrorHandling/InvalidExpressionException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_basic_Project1_CleanArch_Calculator.App.Services.ErrorHandling
+{
+    public class InvalidExpressionException : Exception
+    {
+        public InvalidExpressionException(string message) : base(message) { }
+    }
+
+}
