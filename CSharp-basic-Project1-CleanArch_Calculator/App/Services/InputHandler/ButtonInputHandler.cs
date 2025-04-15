4122236b185a377abd7848ebcb6a0fd635d1477c
mrrajabali <mrrajabali@gmail.com>
2025-04-15 23:37:44 +0330

    Add ButtonInputHandler and KeyboardInputHandler classes
    Introduce `ButtonInputHandler` and `KeyboardInputHandler` classes to handle input from buttons and keyboard keys, respectively. The `ButtonInputHandler` processes button clicks, while the `KeyboardInputHandler` converts key presses into string inputs using a switch expression. Commented-out code in `KeyboardInputHandler` provides an alternative method for key conversion.

---
+++ CSharp-basic-Project1-CleanArch_Calculator/App/Services/InputHandler/ButtonInputHandler.cs
@@ -0,0 +1,23 @@
+﻿using CSharp_basic_Project1_CleanArch_Calculator.App.Services.InputProcessor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_basic_Project1_CleanArch_Calculator.App.Services.InputHandler
+{
+    public class ButtonInputHandler(InputProcessorTest inputProcessor)
+    {
+        private readonly InputProcessorTest _inputProcessor = inputProcessor;
+
+
+        public void ButtonManager(object sender)
+        {
+            if (sender is Button button && button.Tag is string input && !string.IsNullOrEmpty(input))
+            {
+                _inputProcessor.AddInput(input);
+            }
+        }
+    }
+}
