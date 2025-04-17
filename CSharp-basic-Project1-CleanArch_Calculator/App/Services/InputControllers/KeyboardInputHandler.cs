2a4528f48780c460530830e50b62501a2eba13fb
MR. Rajabali <mrrajabali@gmail.com>
2025-04-17 17:04:16 +0330

    refactor: rename folders [InputHandler] to [InputControllers] and move classes for better readiability

--- CSharp-basic-Project1-CleanArch_Calculator/App/Services/InputControllers/KeyboardInputHandler.cs
+++ CSharp-basic-Project1-CleanArch_Calculator/App/Services/InputControllers/KeyboardInputHandler.cs
@@ -45,47 +45,6 @@ namespace CSharp_basic_Project1_CleanArch_Calculator.App.Services.InputHandler
             Keys.Escape => "ESCAPE",
             Keys.Space => string.Empty,
             _ => string.Empty,
-        };
-
-        //private string ConvertKeyToInput(Keys keyCode, bool isShiftPressed)
-        //{
-        //    switch (keyCode)
-        //    {
-        //        case Keys.D0: return "0";
-        //        case Keys.D1: return "1";
-        //        case Keys.D2: return "2";
-        //        case Keys.D3: return "3";
-        //        case Keys.D4: return "4";
-        //        case Keys.D5: return "5";
-        //        case Keys.D6: return "6";
-        //        case Keys.D7: return "7";
-        //        case Keys.D8: return "8";
-        //        case Keys.D9: return "9";
-
-        //        case Keys.NumPad0: return "0";
-        //        case Keys.NumPad1: return "1";
-        //        case Keys.NumPad2: return "2";
-        //        case Keys.NumPad3: return "3";
-        //        case Keys.NumPad4: return "4";
-        //        case Keys.NumPad5: return "5";
-        //        case Keys.NumPad6: return "6";
-        //        case Keys.NumPad7: return "7";
-        //        case Keys.NumPad8: return "8";
-        //        case Keys.NumPad9: return "9";
-        //        case Keys.Decimal: return ".";
-
-        //        case Keys.Add: return "+";
-        //        case Keys.Subtract: return "-";
-        //        case Keys.Multiply: return "*";
-        //        case Keys.Divide: return "/";
-
-        //        case Keys.Enter: return "ENTER";
-        //        case Keys.Back: return "BACKSPACE";
-        //        case Keys.Delete: return "DELETE";
-        //        case Keys.Escape: return "ESCAPE";
-
-        //        default: return string.Empty;
-        //    }
-        //}
+        };       
     }
 }
