147ef8e246529a2fe9f5587e12994ee8b3614808
MR. Rajabali <mrrajabali@gmail.com>
2025-04-17 23:59:01 +0330

    Refactor InputEventHandler for improved structure
    Updated constructor to initialize private fields for
    KeyboardInputHandler, ButtonInputHandler, and DisplayManager.
    Added OnExitRequested event and ExitApplication method.
    Adjusted KeyDownEventHandler for better input processing.

--- CSharp-basic-Project1-CleanArch_Calculator/App/Services/InputEventHandler/InputEventHandler.cs
+++ CSharp-basic-Project1-CleanArch_Calculator/App/Services/InputEventHandler/InputEventHandler.cs
@@ -1,16 +1,27 @@
 ﻿using CSharp_basic_Project1_CleanArch_Calculator.App.Services.InputHandler;
 using CSharp_basic_Project1_CleanArch_Calculator.App.Services.DisplayManagers;
+using CSharp_basic_Project1_CleanArch_Calculator.App.Services.InputControllers;
 
 namespace CSharp_basic_Project1_CleanArch_Calculator.App.Services.InputEventHandler
 {
-    public class InputEventHandler(KeyboardInputHandler keyboardInputHandler, ButtonInputHandler buttonInputHandler,
-                                       DisplayManager displayManager)
+    public class InputEventHandler
     {
-        private readonly KeyboardInputHandler _keyboardInputHandler = keyboardInputHandler;
-        private readonly ButtonInputHandler _buttonInputHandler = buttonInputHandler;
-        private readonly DisplayManager _displayManager = displayManager;
+        private readonly KeyboardInputHandler _keyboardInputHandler;
+        private readonly ButtonInputHandler _buttonInputHandler;
+        private readonly DisplayManager _displayManager;
 
         public event Action<string, string>? OnDataProcessed;
+        public event Action? OnExitRequested;
+
+        public InputEventHandler(KeyboardInputHandler keyboardInputHandler, ButtonInputHandler buttonInputHandler,
+                                 DisplayManager displayManager)
+        {
+            _keyboardInputHandler = keyboardInputHandler;
+            _buttonInputHandler = buttonInputHandler;
+            _displayManager = displayManager;
+            _keyboardInputHandler.ExitHandler += ExitApplication;
+        }
+
 
         public void KeyDownEventHandler(object? sender, KeyEventArgs e)
         {
@@ -38,6 +49,11 @@ namespace CSharp_basic_Project1_CleanArch_Calculator.App.Services.InputEventHand
             string inputData = _displayManager.DisplayInputData();
             string resultData = _displayManager.DisplayResultData();
             OnDataProcessed?.Invoke(inputData, resultData);
+        }
+
+        public void ExitApplication()
+        {
+            OnExitRequested?.Invoke();
         }
     }
 }
